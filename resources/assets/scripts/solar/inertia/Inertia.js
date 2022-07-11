e62c37fe7606dd0c1e350037d6a79f557f0ea78d
Adrián Salvatori <adriansalvatori@gmail.com>
2022-07-11 05:11:30 -0500

    Starting Solar Design System Implementation

--- resources/assets/scripts/solar/inertia/Inertia.js
+++ resources/assets/scripts/solar/inertia/Inertia.js
@@ -1,7 +1,19 @@
 import { jarallax, jarallaxVideo } from "jarallax";
+import LocomotiveScroll from 'locomotive-scroll';
 
 export const Inertia = {
   init: function(){
+
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-inertia-container]'),
+      name: 'inertia',
+      repeat: true,
+      offset: ['10%','10%'],
+      smooth: true,
+      getDirection: true,
+      getSpeed: true,
+    });
+
 
     jarallaxVideo();
     jarallax(document.querySelectorAll('.is-parallax-contain'), {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,15 @@
 @include('partials.header')
 
-<div class="scrollContainer">
-  <main class="main">
+<div data-inertia-container>
+  <main data-inertia-section class="main">
     @yield('content')
   </main>
-  
+
   @hasSection('sidebar')
   <aside class="sidebar">
     @yield('sidebar')
   </aside>
   @endif
-  
-  @include('partials.footer')  
+
+  @include('partials.footer')
 </div>
