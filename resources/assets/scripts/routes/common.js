64679355b4a019d577c51a8240aeff45c0d638b7
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-06-24 16:43:58 -0400

    Star button

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -2,6 +2,11 @@ import {Solar} from '../solar';
 
 export default {
   init() {
+    //codigo para volver atras en la pagina
+    window.addEventListener('popstate', function(event){
+      const to = event.state.states[event.state.index].url;
+      location.assign(to);
+    }, false);
   },
   finalize() {
     Solar.start();
--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -18,6 +18,18 @@
               </div>
             </div>
           </div>
+          {{-- Estructura principal de el Boton estrella --}}
+          <div>
+            <a href="{{home_url('')}}" class="button is-inverse hover-estrella">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
+                    <circle cx="13" cy="13" r="12" stroke="white" stroke-width="2"/>
+                </svg>
+                <svg class="svg-estrella" xmlns="http://www.w3.org/2000/svg" width="26" height="12" viewBox="0 0 55 55" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M28.0314 0.00439453C28.7282 14.5804 40.4221 26.2743 54.998 26.9713V27.0373C40.422 27.7342 28.7281 39.4283 28.0314 54.0044H27.9654C27.2687 39.4281 15.5744 27.7338 0.998047 27.0372V26.9713C15.5743 26.2747 27.2686 14.5806 27.9654 0.00439453H28.0314Z" fill="white"/>
+                </svg>
+                <span class="has-margin-left-15">Empecemos</span>
+            </a>  
+          </div>
         </div>
     </div>
 </div>
