ab73be71e28ef79398eb410cdde2c0a28a4449a1
Adrian Salvatori <adriansalvatori@gmail.com>
2023-07-26 20:26:01 -0500

    Update ThemeServiceProvider.php

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Roots\Acorn\Sage\SageServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class ThemeServiceProvider extends SageServiceProvider
 {
@@ -23,6 +24,16 @@ class ThemeServiceProvider extends SageServiceProvider
      */
     public function boot()
     {
+        /** Feather Directive */
+        Blade::directive('feather', function ($expression) {
+            return "<?php echo '<span class=icon><i data-feather='.{$expression}.'></i></span>'; ?>";
+        });
+
+        /** Product Category Directive */
+        Blade::directive('productcat', function ($product) {
+            return "<?php echo wc_get_product_category_list( $product, ', ', '', '' ); ?>";
+        });
+
         parent::boot();
     }
 }
