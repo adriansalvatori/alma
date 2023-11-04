bd6ea483cfad05d851caa919c7471f9eb19ced46
Adrian Salvatori <adriansalvatori@gmail.com>
2023-11-04 16:58:00 +0000

    Create maintainance-mode.blade.php

---
+++ resources/views/components/maintainance-mode.blade.php
@@ -0,0 +1,143 @@
+<div class="is-ancestor is-gapless  hero is-fullheight  has-background-black is-relative">
+  <div class="is-flex">
+    <div class="column is-4 is-width-35 is-12-mobile is-ancestor is-paddingless is-flex" data-inertia
+      data-inertia-reveal data-inertia-delay="500">
+      <div class="tile is-vertical is-parent" style="padding: 0 !important">
+        <div data-inertia data-inertia-reveal
+          class="tile is-child hero is-fullheight is-dark is-relative    backgound-linear"
+          style="height: 100%; opacity: 0.9;">
+          <div class="is-overlay" style="background-size: cover; mix-blend-mode: color-dodge"></div>
+          <div class="hero-head has-padding-left-50 has-padding-top-50 has-padding-left-25-mobile">
+            <div data-inertia="" data-inertia-reveal="" data-inertia-delay="600" class="container">
+              <img src="@asset('images/logo-primary.svg')" alt="11:11™ Logo" width="120">
+            </div>
+          </div>
+          <div class="hero-body">
+            <div class="container">
+              <div class="has-padding-bottom-35 has-padding-right-5">
+                <div class="is-size-7 is-size-6-mobile has-text-primary is-flex align-items-center">
+                  <strong class="has-text-primary is-size-6 is-uppercase has-text-weight-light">Under Construction</strong>
+                  <div class="title-label has-margin-left-10"></div>
+                </div>
+              </div>
+              <h1
+                class="title is-1 is-size-1-desktop is-size-2-tablet is-size-3-mobile has-margin-bottom-60 has-text-light">
+                <div data-inertia data-inertia-reveal data-inertia-delay="600" class="">We’re under</div>
+                <div data-inertia data-inertia-reveal data-inertia-delay="700" class="">Maintainance.</div>
+                <div data-inertia data-inertia-reveal data-inertia-delay="800" class="">We’ll be back soon.</div>
+
+              </h1>
+              <div class="buttons">
+                <a href="#" data-cursor-text="Gallery" class="button is-light has-text-weight-light is-outlined is-uppercase">
+                  <span class="icon has-margin-right-80"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                      viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                      stroke-linejoin="round" class="feather feather-grid">
+                      <rect x="3" y="3" width="7" height="7"></rect>
+                      <rect x="14" y="3" width="7" height="7"></rect>
+                      <rect x="14" y="14" width="7" height="7"></rect>
+                      <rect x="3" y="14" width="7" height="7"></rect>
+                    </svg></span>
+                  <span>Catalogue</span>
+                </a>
+                {{-- <a href="tel:9543984655" data-cursor-text="Contact Us" class="button is-primary is-outlined">
+                    <span class="icon has-margin-right-80"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg></span>
+                    <span>Contact us</span>
+                  </a> --}}
+              </div>
+            </div>
+          </div>
+          <div
+            class="hero-foot columns is-mobile level has-padding-bottom-50 has-padding-left-50 has-padding-right-50 has-padding-left-25-mobile has-padding-right-25-mobile">
+            <div class="has-padding-left-10 container-contact" style="margin-left: -0.25rem;">
+              <a data-cursor-text="Mail Us" href="mailto:info@11:11.la"
+                class="button is-transparent is-primary is-inverted" style="background: transparent;">
+                <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-mail">
+                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                    <polyline points="22,6 12,13 2,6"></polyline>
+                  </svg></span>
+                <span>info@awakenyourmoments.com</span>
+              </a>
+              <a data-cursor-text="Call Us" href="tel:+584127949330"
+                class="button is-transparent is-primary is-inverted" style="background: transparent;">
+                <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-phone">
+                    <path
+                      d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                    </path>
+                  </svg></span>
+                <span>+57 3217949330</span>
+              </a>
+            </div>
+            <div class="is-relative has-margin-top-5 is-hidden" style="z-index: 99;">
+              <a data-cursor-text="Follow Us" href="https://www.instagram.com/awakenyourmoments/" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                  <path
+                    d="M16 1H6C3.23858 1 1 3.23858 1 6V16C1 18.7614 3.23858 21 6 21H16C18.7614 21 21 18.7614 21 16V6C21 3.23858 18.7614 1 16 1Z"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  <path
+                    d="M15 10.37C15.1234 11.2022 14.9812 12.0522 14.5937 12.799C14.2062 13.5458 13.5931 14.1514 12.8416 14.5297C12.0901 14.9079 11.2384 15.0396 10.4077 14.9059C9.57708 14.7723 8.80971 14.3801 8.21479 13.7852C7.61987 13.1902 7.22768 12.4229 7.09402 11.5922C6.96035 10.7615 7.09202 9.90989 7.47028 9.15837C7.84854 8.40685 8.45414 7.79374 9.20094 7.40624C9.94773 7.01874 10.7977 6.87658 11.63 7C12.4789 7.12588 13.2648 7.52146 13.8716 8.1283C14.4785 8.73515 14.8741 9.52107 15 10.37Z"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M16.5 5.5H16.51" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div data-inertia data-inertia-reveal data-inertia-delay="1000"
+      class="column is-4 is-width-30 is-paddingless is-block-desktop display-none" style="display:none;">
+      <div class="is-full-height"
+        style="background: url('https://images.pexels.com/photos/12165306/pexels-photo-12165306.jpeg') center center / cover no-repeat;"></div>
+    </div>
+    <div class="column is-4 is-width-35 is-paddingless is-hidden-1023" data-inertia data-inertia-reveal
+      data-inertia-delay="1500" data-cursor="-primary">
+      <div style="height:50%" class="is-full-width has-background-danger is-flex wrap align-center has-padding-50">
+        <div class="column is-12 is-paddingless has-padding-bottom-35">
+          <div class="is-size-7 is-size-6-mobile has-text-primary is-flex align-items-center">
+            <strong class="has-text-primary is-size-6 is-uppercase has-text-weight-light">Contact us</strong>
+            <div class="title-label  has-margin-left-10"></div>
+          </div>
+        </div>
+        <div class="column is-12 is-paddingless is-size-3 has-padding-bottom-35">
+          <h3 class="title is-1 has-text-dark">Speak with our <br><strong class="has-text-dark">sales team.</strong></h3>
+        </div>
+        <div class="column is-12 is-paddingless">
+          <div class="buttons align-items-center">
+            <a data-cursor-text="Mail Us" href="mailto:info@bau.us.com"
+              class="has-margin-right-40-tablet button is-primary is-fullwidth-touch is-uppercase">
+              <span class="icon has-margin-right-80"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                  stroke-linejoin="round" class="feather feather-mail">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
+                </svg></span>
+              <span>Mail us</span>
+            </a>
+            <p class="is-size-6 is-hidden-touch has-margin-right-40-tablet is-uppercase has-text-primary">or</p>
+            <a data-cursor-text="Call Us" href="tel:+584127949330"
+              class="button is-primary is-outlined is-fullwidth-touch is-uppercase">
+              <span class="icon has-margin-right-80"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                  stroke-linejoin="round" class="feather feather-phone">
+                  <path
+                    d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                  </path>
+                </svg></span>
+              <span>Call us</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div style="height:50%" class="befor is-full-width is-relative is-flex justify-center align-items-center">
+        <div class="is-full-height is-full-width"
+          style="background: url('@asset('images/packaging.png')'); background-size: cover;">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
