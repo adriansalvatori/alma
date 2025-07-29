9bbb2c1dcc68cd5a9fb6f77c79ffc68801fae43c
Adrián Salvatori <adrian.developer@meeteam.co>
2025-07-29 10:28:51 -0500

    Manually firing events in barba

--- web/app/themes/alma/resources/views/components/rts/barba.blade.php
+++ web/app/themes/alma/resources/views/components/rts/barba.blade.php
@@ -11,10 +11,29 @@
         debug: {{ app()->environment('development') ? 'true' : 'false' }},
         logLevel: 'off',
         prefetchIgnore: false,
-        prevent: ({ el }) => el.classList && (el.classList.contains('ab-item') || el.hasAttribute('href') && el.getAttribute('href').includes('wp-admin')),
+        prevent: ({
+            el
+        }) => el.classList && (el.classList.contains('ab-item') || el.hasAttribute('href') && el
+            .getAttribute('href').includes('wp-admin')),
         preventRunning: false,
         timeout: 2e3,
-        transitions: [],
+        transitions: [{
+            enter() {},
+            beforeEnter: ({ next}) => {
+                const matches = next.html.match(/<body.+?class="([^""]*)"/i);
+                document.body.setAttribute('class', (matches && matches.at(1)) ?? '');
+            },
+            afterEnter: () => {
+                window.scrollTo({
+                    top: 0
+                });
+                document.dispatchEvent(new Event('DOMContentLoaded'));
+                window.dispatchEvent(new Event('load'));
+                if (typeof jQuery !== 'undefined') {
+                    jQuery(document).trigger('ready');
+                }
+            }
+        }]
         views: [],
     })
 </script>
