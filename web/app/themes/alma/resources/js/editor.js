34e51d2bf00e8b21ed033754fa1d48ae94a92f74
Adrián Salvatori <adrian.developer@meeteam.co>
2026-02-28 13:31:37 -0500

    feat: dynamically register WordPress blocks from `block.json` files using `serverSideRender`.

--- web/app/themes/alma/resources/js/editor.js
+++ web/app/themes/alma/resources/js/editor.js
@@ -1,5 +1,25 @@
 import domReady from '@wordpress/dom-ready';
 
 domReady(() => {
-  //
+    const blocks = import.meta.glob('../views/blocks/**/block.json', { eager: true });
+
+    for (const path in blocks) {
+        const block = blocks[path].default || blocks[path];
+        if (block.name && window.wp && window.wp.blocks && window.wp.serverSideRender) {
+            window.wp.blocks.registerBlockType(block.name, {
+                title: block.title,
+                category: block.category,
+                icon: block.icon,
+                description: block.description,
+                attributes: block.attributes,
+                supports: block.supports,
+                edit: function (props) {
+                    return window.wp.element.createElement(window.wp.serverSideRender, {
+                        block: block.name,
+                        attributes: props.attributes,
+                    });
+                },
+            });
+        }
+    }
 });
