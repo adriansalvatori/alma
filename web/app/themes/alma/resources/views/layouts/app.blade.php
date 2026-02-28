46fa70d9b7dbf102ca5eadc22771eca9eaeaa21d
Adrián Salvatori <adrian.developer@meeteam.co>
2026-02-28 09:18:28 -0500

    feat: integrate Flux and Livewire, add dark mode custom variant, and include Flux and Livewire scripts.

--- web/app/themes/alma/resources/views/layouts/app.blade.php
+++ web/app/themes/alma/resources/views/layouts/app.blade.php
@@ -1,38 +1,43 @@
 <!doctype html>
 <html @php(language_attributes())>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @php(do_action('get_header'))
     @php(wp_head())
-
+    @fluxAppearance
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-  </head>
+</head>
 
-  <body @php(body_class())>
+<body @php(body_class())>
     @php(wp_body_open())
 
     <div id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
-        {{ __('Skip to content', 'sage') }}
-      </a>
+        <a class="sr-only focus:not-sr-only" href="#main">
+            {{ __('Skip to content', 'sage') }}
+        </a>
 
-      @include('sections.header')
+        @include('sections.header')
 
-      <main id="main" class="main">
-        @yield('content')
-      </main>
+        <main id="main" class="main">
+            @yield('content')
+        </main>
 
-      @hasSection('sidebar')
-        <aside class="sidebar">
-          @yield('sidebar')
-        </aside>
-      @endif
+        @hasSection('sidebar')
+            <aside class="sidebar">
+                @yield('sidebar')
+            </aside>
+        @endif
 
-      @include('sections.footer')
+        @include('sections.footer')
     </div>
 
     @php(do_action('get_footer'))
     @php(wp_footer())
-  </body>
+    @substrateJs
+    @livewireScripts
+    @fluxScripts
+</body>
+
 </html>
