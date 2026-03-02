c707dba59287280e00b7ad30d7468bfd2bac3dd5
Adrián Salvatori <adrian.developer@meeteam.co>
2026-03-01 23:40:52 -0500

    Remove all ACF-related files, including fields, locations, assets, and language files, from the alma-core theme directory.

--- web/app/themes/alma/functions.php
+++ web/app/themes/alma/functions.php
@@ -50,7 +50,7 @@ Application::configure()
 |
 */
 
-collect(['setup', 'filters', 'core'])
+collect(['setup', 'filters'])
     ->each(function ($file) {
         if (!locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
